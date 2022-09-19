d1d144544941b1f49349c5e9e75c388aceeed7fa
john-mirage <chaosdynamix@posteo.net>
2022-09-19 15:24:50 +0200

    feat: add animation timelines from list to items

--- src/components/web-activity-list/web-activity-list.ts
+++ src/components/web-activity-list/web-activity-list.ts
@@ -1,10 +1,20 @@
 import WebActivity from "@components/web-activity/web-activity";
 import activities from "@data/activities.json";
+import { gsap } from "gsap";
 
 class WebActivityList extends HTMLUListElement {
   [key: string]: any;
   #initialMount = true;
+  #webActivities?: WebActivity[];
+  #activities?: AppData.Activity[];
   #webActivity = <WebActivity>document.createElement("li", { is: "web-activity" });
+  #animationTimeline = gsap.timeline({
+    onComplete: () => {
+      console.log("finish")
+      const customEvent = new CustomEvent("period-has-been-updated", { bubbles: true });
+      this.dispatchEvent(customEvent);
+    }
+  });
 
   static get observedAttributes() {
     return ["data-period"];
@@ -26,20 +36,47 @@ class WebActivityList extends HTMLUListElement {
     }
   }
 
+  get webActivities(): WebActivity[] | undefined {
+    return this.#webActivities;
+  }
+
+  set webActivities(newWebActivities: WebActivity[] | undefined) {
+    this.#webActivities = newWebActivities;
+    if (this.#webActivities) {
+      this.replaceChildren(...this.#webActivities);
+    } else {
+      this.replaceChildren();
+    }
+  }
+
+  get activities(): AppData.Activity[] | undefined {
+    return this.#activities;
+  }
+
+  set activities(newActivities: AppData.Activity[] | undefined) {
+    this.#activities = newActivities;
+    if (this.#activities) {
+      this.webActivities = this.#activities.map((activity) => {
+        const webActivity = <WebActivity>this.#webActivity.cloneNode(true);
+        webActivity.activity = activity;
+        webActivity.period = this.period;
+        webActivity.animationTimeline = this.#animationTimeline;
+        return webActivity;
+      });
+    } else {
+      this.webActivities = undefined;
+    }
+  }
+
   connectedCallback() {
     if (this.#initialMount) {
       this.classList.add("web-activity-list");
       this.#initialMount = false;
     }
     this.upgradeProperty("period");
-    this.replaceChildren(
-      ...activities.map((activity) => {
-        const webActivity = <WebActivity>this.#webActivity.cloneNode(true);
-        webActivity.activity = activity;
-        webActivity.period = this.period;
-        return webActivity;
-      })
-    );
+    this.upgradeProperty("webActivities");
+    this.upgradeProperty("activities");
+    this.activities = activities;
   }
 
   upgradeProperty(prop: string) {
@@ -53,13 +90,9 @@ class WebActivityList extends HTMLUListElement {
   attributeChangedCallback(name: string, _oldValue: string | null, newValue: string | null) {
     switch (name) {
       case "data-period":
-        if (this.children.length > 0) {
-          const webActivities = <WebActivity[]>Array.from(this.children);
-          console.log("web activity list period", webActivities);
-          webActivities.forEach((webActivity) => {
-            webActivity.period = newValue !== null ? newValue : undefined;
-          });
-        }
+        this.webActivities?.forEach((webActivity) => {
+          webActivity.period = newValue ? newValue : undefined;
+        });
         break;
       default:
         throw new Error("The modified attribute is not valid");
